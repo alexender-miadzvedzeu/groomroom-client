8a791e6c328ded914f710d2928e0610532f6fe78
Alexender <medved3993@gmail.com>
2021-06-20 19:10:29 +0300

    add viewer to gallary

--- src/components/Main/Gallery/ImagesContainerWrapper/ImagesContainer/Image/Image.jsx
+++ src/components/Main/Gallery/ImagesContainerWrapper/ImagesContainer/Image/Image.jsx
@@ -1,16 +1,17 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
 import classes from './Image.module.css';
 import SampleNextArrow from "../SampleArrows/SampleNextArrow.jsx";
 import SamplePrevArrow from "../SampleArrows/SamplePrevArrow.jsx";
+import ImageViewer from "react-simple-image-viewer";
 
 const Image = props => {
-    
-    const imagesList = useRef()
+
+    const imagesList = useRef();
 
     const setPhotos = photos => {
         props.setPhotos(photos);
     }
-    
+
     const moveimagesList = value => {
         imagesList.current.scrollLeft += value;
     }
@@ -19,20 +20,52 @@ const Image = props => {
         await fetch(`${process.env.REACT_APP_URL}/images`)
             .then(response => response.json())
             .then(data => setPhotos(data))
+
     }, []);
 
+
+    const [currentImage, setCurrentImage] = useState(0);
+    const [isViewerOpen, setIsViewerOpen] = useState(false);
+    const images = [];
+    const openImageViewer = useCallback((index) => {
+        setCurrentImage(index);
+        setIsViewerOpen(true);
+    }, []);
+
+    const closeImageViewer = () => {
+        setCurrentImage(0);
+        setIsViewerOpen(false);
+    };
     return (
         <div className={classes.wrapper}>
             <div ref={imagesList} className={classes.imageList}>
                 {props.photos.map((elem, key) => {
+                    images.push(elem.image_url)
                     return (
                         <div className={classes.imageWrapper} key={key} >
                             <div className={classes.imgConteiner}>
-                                <img className={classes.image} src={elem.image_url} alt={elem.image_url} />
+                                <img onClick={() => openImageViewer(key)} key={key} className={classes.image} src={elem.image_url} alt={elem.image_url} />
                             </div>
                         </div>
                     )
                 })}
+
+                {isViewerOpen && (
+                    <div className={classes.viewer}>
+                        <ImageViewer
+                            src={images}
+                            currentIndex={currentImage}
+                            onClose={closeImageViewer}
+                            backgroundStyle={{
+                                position: 'fixed',
+                                top: '60px',
+                                zIndex: '120',
+                                backgroundColor: "rgba(0,0,0,0.9)"
+                            }}
+                        />
+                    </div>
+                )}
+
             </div>
             <div className={classes.arrowBox}>
                 <SampleNextArrow onClick={() => moveimagesList(-150)} />
